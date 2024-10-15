0f443db8be8f25ba25c87ac065eb7bcf6689c7cd
AllAcacia <dsh117@uclive.ac.nz>
2024-10-15 14:31:49 +1300

    Doc-string...

--- Match.py
+++ Match.py
@@ -1,6 +1,6 @@
 """
 Authored by AllAcacia on 15/10/2024 (dd/mm/yyyy)
-match.py | "match" class file, contains definitions of a match's outcome.
+match.py | "match" enum file, contains definitions of a match's outcome.
 Intended to be used for in-game logic too, implemented as needed.
 """
 
